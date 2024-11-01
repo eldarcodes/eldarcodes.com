43b95107803cbdd6ea69e1128b1ca46d26403bf2
eldarcodes <prog.eldar@gmail.com>
2024-11-01 23:05:31 +0200

    style: footer to bottom

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 import { About } from "@/components/about";
 import { Footer } from "@/components/footer";
 import { Header } from "@/components/header";
@@ -15,7 +17,37 @@ export default function HomePage() {
           <main id="content" className="pt-10 lg:w-1/2 lg:py-24">
             <About />
 
-            <div className="pt-8 lg:pt-24">
+            <div className="my-5">
+              <Link
+                className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300 group/link text-base"
+                href="/resume.pdf"
+                target="_blank"
+                rel="noreferrer noopener"
+                aria-label="View Résumé (opens in a new tab)"
+              >
+                <span>
+                  View Full{" "}
+                  <span className="inline-block">
+                    Résumé
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px"
+                      aria-hidden="true"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+                        clip-rule="evenodd"
+                      />
+                    </svg>
+                  </span>
+                </span>
+              </Link>
+            </div>
+
+            <div className="absolute bottom-10">
               <Footer />
             </div>
           </main>
--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className="max-w-md pb-16 text-sm text-slate-500 sm:pb-0">
+    <footer className="max-w-md text-sm text-slate-500 sm:pb-0">
       <p>
         Loosely designed in{" "}
         <a
